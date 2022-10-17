9ac9ad65002847a6dc1b7c90145c4be624a1b7d5
Lewis Benson <tm.lewis.benson@gmail.com>
2022-10-17 18:12:23 -0400

    add data to app; add all required html elements with placeholders

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 export default class HornedBeast extends Component {
   render() {
     return (
-      <div>HornedBeast</div>
+      <>
+        <h2>Title of Beast</h2>
+        <img src={null} alt={null} title={null}></img>
+        <p>{null}placeholder</p>
+      </>
     )
   }
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
-
+import HornedBeast from './HornedBeast'
 export default class Main extends Component {
   render() {
     return (
-      <div>Main</div>
+      <>
+        <HornedBeast />
+        <HornedBeast />
+      </>
     )
   }
 }
