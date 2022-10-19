07af43a9c9ecd95c7f5fe287b021865dc1e6dabe
Lewis Benson <tm.lewis.benson@gmail.com>
2022-10-19 19:07:37 -0400

    add function to App to bring in props from child

--- src/App.js
+++ src/App.js
@@ -7,15 +7,33 @@ import data from './data.json'
 import SelectedBeast from './components/SelectedBeast'
 
 export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      show: false,
+      alt: ''
+    }
+  }
+  changeShow = (alt) => {
+    this.setState({
+      show: true,
+      alt: alt
+    })
+  }
 
+  changeClose = () => {
+    this.setState({
+      show: false,
+    })
+  }
 
 
   render() {
     return (
       <>
         <Header />
-        <SelectedBeast data={data}/>
-        <Main className={styles.main} data={data} />
+        <SelectedBeast onCloseModal={this.changeClose} showBeast={data.filter(item => item.title === this.state.alt)} show={this.state.show} data={data} />
+        <Main changeShow={this.changeShow} className={styles.main} data={data} />
 
         <Footer className={styles['page-footer']} />
       </>
